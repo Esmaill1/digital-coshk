bceb5a92fb1e22139c4eda4dc3aec7cf1ef3e932
Esmaill1 <moh12345678912mmo@gmail.com>
2026-01-02 17:10:23 +0200

    Update product grid to be 2 columns on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,7 +111,7 @@ export default function Home() {
             </div>
           </div>
           
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 md:gap-8">
             {isLoading ? (
               <div className="col-span-full flex justify-center py-20">
                 <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
